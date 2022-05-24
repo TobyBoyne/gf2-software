9c7b2b2c20c5c9e783a168e57a76c99752dd6f6e
TobyBoyne <tobyboyne@gmail.com>
2022-05-24 10:42:59 +0100

    Add initial definition of Parse class

--- parse.py
+++ parse.py
@@ -9,6 +9,8 @@ Classes
 Parser - parses the definition file and builds the logic network.
 """
 
+from scanner import Scanner
+from errorlog import ErrorLog
 
 class Parser:
 
@@ -35,10 +37,82 @@ class Parser:
 
     def __init__(self, names, devices, network, monitors, scanner):
         """Initialise constants."""
+        self.names = names
+        self.devices = devices
+        self.netword = network
+        self.monitors = monitors
+        self.scanner: Scanner = scanner
+        self.errorlog = ErrorLog()
+
+        self.symbol = None
+
+    def next_symbol(self):
+        """Scans the next symbol, and assigns it to `self.symbol`"""
+        self.symbol = self.scanner.get_symbol()
+
+    def error(self, errortype, errormessage):
+        self.errorlog(errortype, errormessage)
 
     def parse_network(self):
         """Parse the circuit definition file."""
         # For now just return True, so that userint and gui can run in the
         # skeleton code. When complete, should return False when there are
         # errors in the circuit definition file.
+
+
         return True
+
+    def device(self):
+        pass
+
+    def connection(self):
+        self.outputname()
+        if self.symbol.type == self.scanner.EQUALS:
+            self.symbol = self.scanner.get_symbol()
+            self.inputname()
+        else:
+            self.errorlog()
+
+    def devicename(self):
+        pass
+
+    def inputname(self):
+        self.devicename()
+        self.next_symbol()
+        if self.symbol.type == Scanner.DOT:
+            self.next_symbol()
+            # TODO: ensure this is LL(1) compliant
+            if self.symbol.type == Scanner.NAME:
+                if self.symbol.id[0] == "I" and self.symbol.id[1:].isalnum():
+                    self.next_symbol
+                    pass
+                else:
+                    self.error(None, "Must be a valid input")
+            else:
+                self.error(None, "Symbol must be a name")
+        else:
+            self.error(None, "Not an input - must have a dot")
+
+    def outputname(self):
+        self.devicename()
+        self.next_symbol()
+        if self.symbol.type == Scanner.DOT:
+            self.next_symbol()
+            if self.symbol.type == Scanner.NAME:
+                if self.symbol.id in Scanner.OUTPUTS_LIST:
+                    self.next_symbol()
+                    pass
+                else:
+                    self.error(None, f"Must be a valid output name {Scanner.OUTPUTS_LIST}")
+            else:
+                self.error(None, "Symbol must be a name")
+            
+
+    def comment(self):
+        """Ignore all symbols between two slash symbols"""
+        if self.symbol.type == Scanner.SLASH:
+            self.next_symbol()
+            while self.symbol.type != Scanner.SLASH:
+                self.next_symbol()
+        
+        self.next_symbol()
