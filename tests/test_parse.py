bf50bbb9ddd9112d422ed30f68b51f34a30c7cdd
TobyBoyne <tobyboyne@gmail.com>
2022-06-02 00:25:22 +0100

    Refactor test_parse to better use fixtures

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -8,81 +8,66 @@ from scanner import Scanner
 from parse import Parser
 
 
-@pytest.fixture(scope="module")
-def names():
-    """Return a new Names instance."""
-    return Names()
-
-
-@pytest.fixture(scope="module")
-def devices(names):
-    """Return a Devices instance."""
-    return Devices(names)
-
-
-@pytest.fixture(scope="module")
-def network(names, devices):
-    """Return a Network instance."""
-    return Network(names, devices)
-
-
-@pytest.fixture(scope="module")
-def monitors(names, devices, network):
-    """Return a new Monitors instance."""
-    return Monitors(names, devices, network)
-
-
 def new_file(tmpdir, file_contents):
     """Return a file path to a mock file used in testing"""
     p = tmpdir.mkdir("sub").join("example.txt")
     p.write(file_contents)
     return p
 
+@pytest.fixture(scope="function")
+def parser(tmpdir, request):
+    """Create a new parser object
+    Note that the scope is *not* module, as a new Parser is required
+    for each different file path"""
+    path = new_file(tmpdir, request.param)
+
+    names = Names()
+    devices = Devices(names)
+    network = Network(names, devices)
+    monitors = Monitors(names, devices, network)
+    scanner = Scanner(path, names)
+    return Parser(names, devices, network, monitors, scanner)
+
+
+@pytest.mark.parametrize('parser', ['filecontents'], indirect=True)
+def test_parser_creation(parser):
+    """Create a parser object using fixtures"""
+    assert isinstance(parser, Parser)
+
+
 
 devicename_test_files_1 = ["DEVICE", "CONNECT", "MONITOR", "INPUTS"]
 
 
-@pytest.mark.parametrize("file_contents", devicename_test_files_1)
-def test_parse_raises_semantic_errors_1(file_contents, tmpdir, names,
-                                        devices, network, monitors):
+@pytest.mark.parametrize("parser", devicename_test_files_1, indirect=True)
+def test_parse_raises_semantic_errors_1(parser):
     """Test if the parser correctly raises a ProtectedKeywordError."""
-    path = new_file(tmpdir, file_contents)
-    scanner = Scanner(path, names)
-    parse = Parser(names, devices, network, monitors, scanner)
-    parse.next_symbol()
+    parser.next_symbol()
     with pytest.raises(errorlog.ProtectedKeywordError):
-        parse.devicename()
+        parser.devicename()
 
 
 devicename_test_files_2 = [",NOR", ":G1:", "0SW", "\t;CLK1"]
 
 
-@pytest.mark.parametrize("file_contents", devicename_test_files_2)
-def test_parse_raises_syntax_errors_1(file_contents, tmpdir, names,
-                                      devices, network, monitors):
+@pytest.mark.parametrize("parser", devicename_test_files_2, indirect=True)
+def test_parse_raises_syntax_errors_1(parser):
     """Test if the parser correctly raises a NameSyntaxError.
 
     Error occurs when a name does not start at a letter"""
-    path = new_file(tmpdir, file_contents)
-    scanner = Scanner(path, names)
-    parse = Parser(names, devices, network, monitors, scanner)
-    parse.next_symbol()
+    parser.next_symbol()
     with pytest.raises(errorlog.NameSyntaxError):
-        parse.devicename()
+        parser.devicename()
 
 
 inputname_test_files = ["G1,I1", "G2:I2", "G3;I3", "G4 I4"]
 
 
-@pytest.mark.parametrize("file_contents", inputname_test_files)
-def test_parse_raises_syntax_errors_2(file_contents, tmpdir, names,
-                                      devices, network, monitors):
+@pytest.mark.parametrize("parser", inputname_test_files, indirect=True)
+def test_parse_raises_syntax_errors_2(parser):
     """Test if the parser correctly raises a PunctuationError.
 
     Occurs when an input does not start with a dot. """
-    path = new_file(tmpdir, file_contents)
-    scanner = Scanner(path, names)
-    parse = Parser(names, devices, network, monitors, scanner)
-    parse.next_symbol()
+    parser.next_symbol()
     with pytest.raises(errorlog.PunctuationError):
-        parse.inputname()
+        parser.inputname()
